e407bc3bc5fb07e1e101b77dcf226fa462c0ab9c
Timothy Bolton <bolton.timothy@gmail.com>
2022-04-29 12:21:45 -0500

    Add files

---
+++ README.md
@@ -0,0 +1,27 @@
+# twitch-chat-logger
+Logs Twitch chatrooms.
+
+This is useful for twitch streams that don't save their videos.
+
+Combining this with software for stream backups gives all the necessary info to replicate the experience.
+
+Note: This is pretty heavily inspired from other projects. Nothing novel here.
+
+# Setup
+
+Create an OAuth token to authenticate with Twitch IRC by visiting here: https://twitchapps.com/tmi/
+
+Create a `.env` file and add the OAuth Token into it:
+
+    twitch_token = 'oauth:TheTokenValueShouldGoRightHere'
+
+# Usage
+    usage: twitch-chat-logger.py [-h] [-o OUTPUT] [-q] -c CHANNEL
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      -o OUTPUT, --output OUTPUT
+                            File to write to, if not provided, it will default to a timestamp
+      -v, --verbose         Show chat output
+      -c CHANNEL, --channel CHANNEL
+                            Channel to log
---
+++ twitch-chat-logger.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv
+import os
+import socket
+import logging
+from emoji import demojize
+import argparse
+import time
+
+load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--output", help="File to write to, if not provided, it will default to a timestamp")
+parser.add_argument("-v", "--verbose", help="Show chat output", action="store_true")
+parser.add_argument("-c", "--channel", help="Channel to log", required=True)
+
+args = parser.parse_args()
+
+is_verbose = args.verbose
+twitch_channel = args.channel
+file_name = args.output if args.output else f"{twitch_channel}_{int(time.time())}.log"
+
+server = 'irc.chat.twitch.tv'
+port = 6667
+nick = os.environ.get('twitch_user')
+token = os.environ.get('twitch_token')
+irc_channel = f"#{twitch_channel}"
+
+sock = socket.socket()
+sock.connect((server, port))
+sock.send(f"PASS {token}\n".encode('utf-8'))
+sock.send(f"NICK {nick}\n".encode('utf-8'))
+sock.send(f"JOIN {irc_channel}\n".encode('utf-8'))
+
+logging.basicConfig(level=logging.DEBUG,
+        format='%(asctime)s - %(message)s',
+        datefmt='%Y-%m-%d_%H:%M:%S',
+
+        handlers=[logging.FileHandler(file_name, encoding='utf-8')])
+
+while True:
+    response = sock.recv(2048).decode('utf-8')
+    if response.startswith('PING'):
+        sock.send("PONG\n".encode('utf-8'))
+
+    elif len(response) > 0:
+        logging.info(response)
+        if is_verbose:
+            print(f"{response}\n")
+
